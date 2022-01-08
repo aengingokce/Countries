d831f31714116379bb63e82dca9a5b10754daac7
Ahmet Engin Gökçe <aengingokce@gmail.com>
2022-01-09 01:37:18 +0300

    Update Constants.swift
    Api key was removed

--- Countries/Utils/Constants.swift
+++ Countries/Utils/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 var host = "wft-geo-db.p.rapidapi.com"
-var key = "d746be4c41msh1bf18494fe449ffp1ad3f3jsn56c43e679620"
+var key = "YOUR_API_KEY"
 var uri = "https://wft-geo-db.p.rapidapi.com/v1/geo/countries"
 var limit = "?offset=0&limit=10"
 
